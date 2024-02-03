448c95791ef41c699487f61ac7caa4265f3b9af2
MattiaFiocchi <mattia.fiocchi47@gmail.com>
2024-02-03 08:25:21 +0100

    Update backend.js

--- Backend/backend.js
+++ Backend/backend.js
@@ -73,9 +73,4 @@ app.get('/prenotazioni_libri',(req,res) => {
 
 app.listen(3000, () => {
     console.log('server_startato');
-<<<<<<< HEAD
 });
-
-=======
-});
->>>>>>> 31fd891e07a1ba6ce6e243f990a6b1a1c8628578
