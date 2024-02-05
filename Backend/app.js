da99eac3e8e8713e9e6dc5ebb0ed8a708db486a7
MattiaFiocchi <mattia.fiocchi47@gmail.com>
2024-02-05 11:23:09 +0100

    modifiche app.js

--- Backend/app.js
+++ Backend/app.js
@@ -67,10 +67,7 @@ const ListaLibri = [
     },
     // ... altri libri ...
 ];
-
-app.get('/prenotazioni_libri', (req, res) => {
-    res.status(200).json(ListaLibri);
-});
+/*
 
 app.post('/aggiungi_libro', (req, res) => {
     const nuovoLibro = req.body;
@@ -84,6 +81,55 @@ app.post('/aggiungi_libro', (req, res) => {
     ListaLibri.push(nuovoLibro);
 
     res.status(201).json({ message: 'Libro aggiunto con successo.', libro: nuovoLibro });
+});
+*/
+app.get('/prenotazioni_libri', (req, res) => {
+    res.status(200).json(ListaLibri);
+});
+
+app.get('/ListaLibri/:id', (req, res) => {
+    let id = req.params.id;
+    let libro = ListaLibri.find((item) => item.id == id);
+
+    if (libro != undefined)
+        res.json(libro);
+    else
+        res.status(404).send("Libro non trovato");
+});
+
+app.post('/ListaLibri', (req, res) => {
+    console.log(req.body);
+    let TitoloLibro = req.body.TitoloLibro;
+    let Autore = req.body.Autore;
+    let Genere = req.body.Genere;
+
+    if (TitoloLibro && Autore && Genere) {
+        let id = ListaLibri[ListaLibri.length - 1].id + 1;
+        const nuovoLibro = {
+            id,
+            TitoloLibro,
+            Autore,
+            Genere
+        };
+
+        ListaLibri.push(nuovoLibro);
+        res.status(201).json(nuovoLibro);
+    } else {
+        res.status(400).send("Formato del body errato");
+    }
+});
+
+app.put('/ListaLibri/:id', (req, res) => {
+    let id = req.params.id;
+    let libroIndex = ListaLibri.findIndex((item) => item.id == id);
+
+    if (libroIndex >= 0) {
+        let updatedLibro = req.body;
+        ListaLibri[libroIndex] = { ...ListaLibri[libroIndex], ...updatedLibro };
+        res.json(ListaLibri[libroIndex]);
+    } else {
+        res.status(404).send("Libro non trovato");
+    }
 });
 
 app.listen(port, () => {
